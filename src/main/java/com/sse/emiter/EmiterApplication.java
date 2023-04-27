32798e163ffece373e2da4a75d9c81c204af265a
Ion Ionets <ion@lehit.care>
2023-04-27 12:11:56 +0800

    Initial commit

---
+++ src/main/java/com/sse/emiter/EmiterApplication.java
@@ -0,0 +1,13 @@
+package com.sse.emiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sse/emiter/EmiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sse.emiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
