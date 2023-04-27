d20d8f19fc3e54661072cd1cf45193da5234b8da
Ion Ionets <ion@lehit.care>
2023-04-27 20:50:45 +0800

    cleanup

--- src/main/java/com/sse/emiter/client/OpenAiReactiveClient.java
+++ src/main/java/com/sse/emiter/client/OpenAiReactiveClient.java
@@ -1,38 +1,38 @@
 package com.sse.emiter.client;
 
+import com.sse.emiter.model.GptResponse;
+import com.sse.emiter.model.MessageRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
-import java.time.LocalTime;
 import java.util.Map;
 
 @Component
 @Slf4j
 public class OpenAiReactiveClient {
 
+    public Flux<GptResponse> requestCompletion(MessageRequest request) {
 
-    public void consumeServerSentEvent() {
-        WebClient client = WebClient.create("http://localhost:8085/sse-server");
-//		WebClient client = WebClient.create("http://localhost:8080/sse-server");
+        WebClient client = WebClient.create("http://localhost:8080/sse-server");
 
-        ParameterizedTypeReference<ServerSentEvent<String>> type
-                = new ParameterizedTypeReference<ServerSentEvent<String>>() {};
+        ParameterizedTypeReference<GptResponse> type
+                = new ParameterizedTypeReference<>() {
+        };
 
-        Flux<ServerSentEvent<String>> eventStream = client.post()
+        var eventStream = client.post()
                 .uri("/stream-gpt")
-                .body(BodyInserters.fromValue(Map.of("content", "dough")))
+                .body(BodyInserters.fromValue(Map.of("content", request.content())))
                 .retrieve()
-                .bodyToFlux(type);
+                .bodyToFlux(type)
+                .doOnNext(content -> log.info("Received SSE event: name[{}], id [{}], content[{}]",
+                        content.choices().get(0).finishReason(),
+                        content.choices().get(0).index(),
+                        content.choices().get(0).delta().content()));
 
-        eventStream.subscribe(
-                content -> log.info("Time: {} - event: name[{}], id [{}], content[{}] ",
-                        LocalTime.now(), content.event(), content.id(), content.data()),
-                error -> log.error("Error receiving SSE: {}", error),
-                () -> log.info("Completed!!!"));
+        return eventStream;
     }
 }
--- src/main/java/com/sse/emiter/controller/SseEmiterController.java
+++ src/main/java/com/sse/emiter/controller/SseEmiterController.java
@@ -1,72 +1,43 @@
 package com.sse.emiter.controller;
 
+import com.sse.emiter.client.OpenAiReactiveClient;
 import com.sse.emiter.model.GptResponse;
 import com.sse.emiter.model.MessageRequest;
-import com.sse.emiter.service.OpenAICompletionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import reactor.core.publisher.Flux;
-
-import java.time.LocalTime;
-import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @RestController
 @RequestMapping("/sse-server")
 @RequiredArgsConstructor
 @Slf4j
 public class SseEmiterController {
-    private final OpenAICompletionService openAICompletionService;
+    private final OpenAiReactiveClient openAICompletionService;
 
-
-
-    @GetMapping("/stream-sse")
-    public SseEmitter streamSseMvc() {
-        SseEmitter emitter = new SseEmitter();
-        ExecutorService sseMvcExecutor = Executors.newFixedThreadPool(6);
-        sseMvcExecutor.execute(() -> {
-            try {
-                for (int i = 0; true; i++) {
-                    SseEmitter.SseEventBuilder event = SseEmitter.event()
-                            .data("SSE MVC - " + LocalTime.now())
-                            .id(String.valueOf(i))
-                            .name("sse event - mvc");
-                    emitter.send(event);
-                    Thread.sleep(2000);
-                }
-            } catch (Exception ex) {
-                emitter.completeWithError(ex);
-            }
-        });
-        return emitter;
-    }
 
     @PostMapping(value = "/stream-gpt-react", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<GptResponse> streamSse(@RequestBody MessageRequest request) {
 
-		WebClient client = WebClient.create("http://localhost:8080/sse-server");
+//		WebClient client = WebClient.create("http://localhost:8080/sse-server");
+//
+//        ParameterizedTypeReference<GptResponse> type
+//                = new ParameterizedTypeReference<>() {
+//        };
+//
+//         var eventStream = client.post()
+//                .uri("/stream-gpt")
+//                .body(BodyInserters.fromValue(Map.of("content", request.content())))
+//                .retrieve()
+//                .bodyToFlux(type)
+//                 .doOnNext(content -> log.info("Received SSE event: name[{}], id [{}], content[{}]",
+//                         content.choices().get(0).finishReason(),
+//                         content.choices().get(0).index(),
+//                         content.choices().get(0).delta().content()));
+//
+//        return eventStream;
 
-        ParameterizedTypeReference<GptResponse> type
-                = new ParameterizedTypeReference<>() {
-        };
-
-         var eventStream = client.post()
-                .uri("/stream-gpt")
-                .body(BodyInserters.fromValue(Map.of("content", request.content())))
-                .retrieve()
-                .bodyToFlux(type)
-                 .doOnNext(content -> log.info("Received SSE event: name[{}], id [{}], content[{}]",
-                         content.choices().get(0).finishReason(),
-                         content.choices().get(0).index(),
-                         content.choices().get(0).delta().content()));
-
-        return eventStream;
+        return openAICompletionService.requestCompletion(request);
     }
 }
